3528a026d73224b08b2016c16714f07858c0fe59
Sakshi Jha <95759285+jha-sakshi@users.noreply.github.com>
2023-09-16 15:19:18 +0530

    Update About.js

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
-import Button from "@material-ui/core/Button";
-
+/*import Button from "@material-ui/core/Button";*/
 import { getHighScore } from "../config/SaveScore";
 
 const styles = StyleSheet.create({
